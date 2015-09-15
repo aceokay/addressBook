464409fd912bc401f63d2d4fc97410511334eddb
Austin <aokincaid@gmail.com>
2015-09-14 22:04:52 -0700

    Add spec to test additional functionality of address prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,20 @@ function Contact(firstName,lastName){
   this.addresses = [];
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
 $(document).ready(function() {
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address jumbotron">' +
@@ -28,7 +42,7 @@ $(document).ready(function() {
    var inputtedFirstName = $("input#new-first-name").val();
    var inputtedLastName = $("input#new-last-name").val();
 
-   var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+   var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
    $(".new-address").each(function() {
      var inputtedStreet = $(this).find("input.new-street").val();
@@ -40,7 +54,7 @@ $(document).ready(function() {
    });
 
 
-   $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+   $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
    $(".contact").last().click(function() {
      $("#show-contact").show();
